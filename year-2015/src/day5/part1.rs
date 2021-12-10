c0424ae0e5a877809b3731873e67921282133c4a
Maxime BAILLY <maxbailly@hotmail.fr>
2021-12-10 19:01:55 +0100

    2015 day 5 : done

---
+++ year-2015/src/day5/part1.rs
@@ -0,0 +1,50 @@
+enum StringType {
+    Nice,
+    Naughty
+}
+
+impl From<&'static str> for StringType {
+    fn from(line: &'static str) -> Self {
+        let mut vowels = 0u8;
+        let mut double = false;
+
+        let mut prev = &b'\0';
+
+        for c in line.as_bytes() {
+            if *prev == b'\0' {
+                prev = c;
+                continue;
+            }
+
+            if c == prev {
+                double = true
+            }
+
+            match c {
+                b'a' | b'e' | b'i' | b'o' | b'u' => vowels += 1,
+                b'b' if *prev == b'a' => return Self::Naughty,
+                b'd' if *prev == b'c' => return Self::Naughty,
+                b'q' if *prev == b'p' => return Self::Naughty,
+                b'y' if *prev == b'x' => return Self::Naughty,
+                _ => ()
+            }
+
+            prev = c;
+        }
+
+        if vowels < 3 || !double {
+            return Self::Naughty
+        }
+
+        Self::Nice
+    }
+}
+
+fn main() {
+    let count = utils::inputs_str!("1").split_whitespace()
+        .map(StringType::from)
+        .filter(|str_type| matches!(str_type, StringType::Nice))
+        .count();
+
+    println!("result = {}", count);
+}

069b78427ec92527389f1a9b571328727b1c76ec
Maxime BAILLY <maxbailly@hotmail.fr>
2021-12-10 18:54:55 +0100

    2015 day 4 : done

--- year-2015/src/day4/part1.rs
+++ year-2015/src/day4/part1.rs
@@ -1,62 +1,21 @@
-use std::thread::{self, JoinHandle};
-use std::sync::mpsc;
-
-const INPUT: &str = "bgvyzdsv";
-
-struct Worker {
-    thread: Option<JoinHandle<()>>,
-    tx: mpsc::Sender<Option<u64>>
-}
-
-impl Worker {
-    fn spawn() -> Self {
-        let (tx, rx) = mpsc::channel();
-
-        let thread = thread::spawn(move || {
-            while let Ok(msg) = rx.recv() {
-                match msg {
-                    Some(_) => (),
-                    None => break
-                }
-            }
-        });
-
-        Self {
-            thread: Some(thread),
-            tx
-        }
-    }
-}
-
-impl Drop for Worker {
-    fn drop(&mut self) {
-        if let Some(thread) = self.thread.take() {
-            let _ = self.tx.send(None);
-            let _ = thread.join();
-        }
-    }
-}
-
-
-
-struct ThreadPool<const N: usize> {
-    workers: Vec<Worker>
-}
-
-impl<const N: usize> ThreadPool<N> {
-    fn new() -> Self {
-        let mut workers = Vec::with_capacity(N);
-
-        for _ in 0..N {
-            workers.push(Worker::spawn());
-        }
-
-        Self {
-            workers
-        }
-    }
-}
+use md5::{Md5, Digest};
 
 fn main() {
-    let tp = ThreadPool::<12>::new();
+    const INPUT: &str = "bgvyzdsv";
+    let mut count = 0;
+    let mut hasher = Md5::new();
+
+    loop {
+        let test = format!("{}{}", INPUT, count);
+
+        hasher.update(test.as_bytes());
+        let result = hasher.finalize_reset();
+
+        if result[0] == 0 && result[1] == 0 && result[2] < 16 {
+            println!("result => {}", count);
+            return ;
+        } else {
+            count += 1;
+        }
+    }
 }

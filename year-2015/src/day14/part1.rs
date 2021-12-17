4a9460b8cc732dd6bd5ff0a53a9b8b37f12fbc8d
Maxime BAILLY <maxbailly@hotmail.fr>
2021-12-17 17:58:54 +0100

    2015 day 14 : part 1

---
+++ year-2015/src/day14/part1.rs
@@ -0,0 +1,44 @@
+const TRAVEL_TIME: u32 = 2503;
+
+/* ---------- */
+
+#[derive(Debug)]
+struct Reindeer {
+    speed: u32,
+    flight_time: u32,
+    rest_time: u32
+}
+
+impl Reindeer {
+    fn new(speed_str: &str, ft_str: &str, rt_str: &str) -> Self {
+        Self {
+            speed: speed_str.parse().expect("failed to parse speed"),
+            flight_time: ft_str.parse().expect("failed to parse flight time"),
+            rest_time: rt_str.parse().expect("failed to parse rest time")
+        }
+    }
+
+    fn distance_at(&self, time: u32) -> u32 {
+        let current_phase = time % (self.flight_time + self.rest_time);
+        let total_phases = time / (self.flight_time + self.rest_time);
+        let additional_flight_time = if current_phase > self.flight_time { self.flight_time } else { current_phase };
+
+        (total_phases * self.flight_time * self.speed) + (additional_flight_time * self.speed)
+    }
+}
+
+/* ---------- */
+
+fn main() {
+    let max = utils::inputs_str!("test").split('\n')
+        .map(|line| {
+            let parts = line.split_whitespace().collect::<Vec<&str>>();
+            let reindeer = Reindeer::new(parts[3], parts[6], parts[13]);
+
+            reindeer.distance_at(TRAVEL_TIME)
+        })
+        .max()
+        .expect("couldn't find max distance");
+
+    println!("result = {}", max);
+}

50c79556bcb1ea5e2d3fd6a4203351e30f4691d1
Maxime BAILLY <maxbailly@hotmail.fr>
2021-12-17 17:37:06 +0100

    2015 day 12 : part 1

---
+++ year-2015/src/day12/part1.rs
@@ -0,0 +1,19 @@
+use serde_json::Value;
+
+/* ---------- */
+
+fn add_numbers(value: &Value) -> i64 {
+    match value {
+        Value::Array(arr) => arr.iter().map(add_numbers).sum(),
+        Value::Object(obj) => obj.iter().map(|(_, val)| add_numbers(val)).sum(),
+        Value::Number(val) => val.as_i64().expect("failed to retrieve json value"),
+        _ => 0
+    }
+}
+
+/* ---------- */
+
+fn main() {
+    let value: Value = serde_json::from_str(utils::inputs_str!("1")).expect("failed to parse");
+    println!("{}", add_numbers(&value));
+}

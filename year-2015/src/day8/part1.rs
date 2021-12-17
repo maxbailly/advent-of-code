262b6cbcba47ba1f36cc15f55d165a287dd7e4af
Maxime BAILLY <maxbailly@hotmail.fr>
2021-12-17 17:32:18 +0100

    2015 day 8 : part 1 done

---
+++ year-2015/src/day8/part1.rs
@@ -0,0 +1,56 @@
+#[inline(always)]
+fn is_ascii_numeric(c: &u8) -> bool {
+    *c >= b'0' && *c <= b'9'
+}
+
+/* ---------- */
+
+fn escape(idx: &mut usize, bytes: &[u8]) {
+    *idx += 1;
+
+    if bytes[*idx] == b'x' {
+        *idx += 1;
+    }
+
+    while *idx < bytes.len() && is_ascii_numeric(&bytes[*idx]) {
+        *idx += 1;
+    }
+}
+
+/* ---------- */
+
+fn count_mem_chars(line: &str, len: usize) -> u8 {
+    let bytes = line.as_bytes();
+    let mut idx = 0usize;
+    let mut count = 0u8;
+
+    while idx < len {
+        match bytes[idx] {
+            b'\\' => {
+                escape(&mut idx, bytes);
+                count += 1
+            },
+            b'"' => (),
+            _ => count += 1
+        }
+
+        idx += 1;
+    }
+
+    count
+}
+
+/* ---------- */
+
+fn main() {
+    let count = utils::inputs_str!("1").split_whitespace()
+        .map(|line| {
+            let code_len = line.len();
+            let count = count_mem_chars(line, code_len);
+
+            code_len - count as usize
+        })
+        .sum::<usize>();
+
+    println!("result = {}", count);
+}

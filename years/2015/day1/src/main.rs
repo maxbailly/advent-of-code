91d8172d65d280f7e72591111ce053bd18c5780e
Maxime BAILLY <maxbailly@hotmail.fr>
2022-01-21 21:28:00 +0100

    2015 day 1 : finished

--- years/2015/day1/src/main.rs
+++ years/2015/day1/src/main.rs
@@ -1,7 +1,7 @@
-fn main() {
-    let input = utils::input_bytes!("part1.txt");
+const INPUT: &[u8] = utils::input_bytes!("part1.txt");
 
-    let ret = input.iter()
+fn part1() -> i32 {
+    INPUT.iter()
         .map(|c | {
             match c {
                 b'(' => 1,
@@ -9,7 +9,32 @@ fn main() {
                 _ => 0
             }
         })
-        .sum::<i32>();
+        .sum::<i32>()
+}
 
-    println!("{}", ret);
+/* ---------- */
+
+fn part2() -> usize {
+    let mut position = 0;
+
+    for (index, character) in INPUT.iter().enumerate() {
+        match character {
+            b'(' => position += 1,
+            b')' => position -= 1,
+            _ => ()
+        }
+
+        if position == -1 {
+            return index + 1
+        }
+    }
+
+    panic!("Santa never went to basement -1")
+}
+
+/* ---------- */
+
+fn main() {
+    println!("[PART 1] Answer = {}", part1());
+    println!("[PART 1] Answer = {}", part2())
 }

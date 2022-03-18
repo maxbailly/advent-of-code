375fbc8cdd0b4179699f4f0af2705b0aa38ab0d7
Maxime BAILLY <maxbailly@hotmail.fr>
2022-03-18 22:58:07 +0100

    2015 day 15 : completed !

--- years/2015/day15/src/main.rs
+++ years/2015/day15/src/main.rs
@@ -1,17 +1,17 @@
 use std::iter::Sum;
-use std::ops::Add;
+use std::ops::{Add, Mul};
 
-use integer_partitions::Partitions;
+use kombini::Kombini;
+use partitions::Partitions;
 
-/* ----------- */
+/* ---------- */
 
-type Ingredients = Vec<Ingredient>;
+const INPUT: &str = utils::input_str!();
 
-/* ----------- */
+/* ---------- */
 
 #[derive(Default, Debug, Copy, Clone)]
-struct Ingredient {
-    _name: &'static str,
+struct Stats {
     capacity: i32,
     durability: i32,
     flavor: i32,
@@ -19,32 +19,24 @@ struct Ingredient {
     calories: i32,
 }
 
-impl Ingredient {
-    fn values_for_teaspoons(&self, teaspoons: i32) -> Self {
-        Self {
-            _name: self._name,
-            capacity: self.capacity * teaspoons,
-            durability: self.durability * teaspoons,
-            flavor: self.flavor * teaspoons,
-            texture: self.texture * teaspoons,
-            calories: self.calories,
-        }
-    }
-
-    fn score(&self) -> i32 {
+impl Stats {
+    fn score(&self) -> u32 {
         if self.capacity <= 0 || self.durability <= 0 || self.flavor <= 0 || self.texture <= 0 {
             return 0;
         }
 
-        self.capacity * self.durability * self.flavor * self.texture
+        (self.capacity * self.durability * self.flavor * self.texture) as u32
+    }
+
+    fn calories(&self) -> i32 {
+        self.calories
     }
 }
 
-impl Add<Self> for Ingredient {
+impl Add for Stats {
     type Output = Self;
     fn add(self, rhs: Self) -> Self::Output {
         Self {
-            _name: "Mixed",
             capacity: self.capacity + rhs.capacity,
             durability: self.durability + rhs.durability,
             flavor: self.flavor + rhs.flavor,
@@ -54,18 +46,30 @@ impl Add<Self> for Ingredient {
     }
 }
 
-impl Sum for Ingredient {
-    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
-        iter.fold(Ingredient::default(), |a, b| a + b)
+impl Mul<usize> for Stats {
+    type Output = Self;
+    fn mul(self, rhs: usize) -> Self::Output {
+        Self {
+            capacity: self.capacity * rhs as i32,
+            durability: self.durability * rhs as i32,
+            flavor: self.flavor * rhs as i32,
+            texture: self.texture * rhs as i32,
+            calories: self.calories * rhs as i32,
+        }
     }
 }
 
-impl From<&'static str> for Ingredient {
+impl Sum for Stats {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(Self::default(), |a, b| a + b)
+    }
+}
+
+impl From<&'static str> for Stats {
     fn from(line: &'static str) -> Self {
         let parts = line.split(": ").collect::<Vec<&str>>();
 
         let mut ret = Self {
-            _name: parts[0],
             capacity: 0,
             durability: 0,
             flavor: 0,
@@ -77,13 +81,13 @@ impl From<&'static str> for Ingredient {
             let parts = part.split_once(' ').expect("a valid ingredient stat");
 
             match parts {
-                ("capacity", val) => ret.capacity = val.parse().expect("(capacity) a valid i8"),
+                ("capacity", val) => ret.capacity = val.parse().expect("(capacity) a valid i32"),
                 ("durability", val) => {
-                    ret.durability = val.parse().expect("(durability) a valid i8")
+                    ret.durability = val.parse().expect("(durability) a valid i32")
                 }
-                ("flavor", val) => ret.flavor = val.parse().expect("(flavor) a valid i8"),
-                ("texture", val) => ret.texture = val.parse().expect("(texture) a valid i8"),
-                ("calories", val) => ret.calories = val.parse().expect("(calories) a valid i8"),
+                ("flavor", val) => ret.flavor = val.parse().expect("(flavor) a valid i32"),
+                ("texture", val) => ret.texture = val.parse().expect("(texture) a valid i32"),
+                ("calories", val) => ret.calories = val.parse().expect("(calories) a valid i32"),
                 _ => panic!("unknown stat"),
             }
         });
@@ -92,70 +96,35 @@ impl From<&'static str> for Ingredient {
     }
 }
 
-/* ----------- */
-
-fn rotate_array<T: Copy>(arr: &mut Vec<T>) {
-    let arr_len = arr.len();
-
-    if arr_len == 0 {
-        return;
-    }
-
-    let first = arr[0];
-    for i in 1..arr_len {
-        arr[i - 1] = arr[i]
-    }
-
-    arr[arr_len - 1] = first
-}
-
-/* ----------- */
-
-fn score(ingrs: &[Ingredient], proportions: &[usize]) -> i32 {
-    let mixed = ingrs
-        .iter()
-        .enumerate()
-        .map(|(idx, ingr)| ingr.values_for_teaspoons(proportions[idx] as i32))
-        .sum::<Ingredient>();
-
-    mixed.score()
-}
-
-/* ----------- */
-
-fn all_proportions_score(ingrs: &[Ingredient], proportions: &[usize]) -> i32 {
-    let mut intermediate_score = 0;
-    let mut prop = Vec::from(proportions);
-
-    for _ in 0..prop.len() {
-        let score = score(ingrs, &prop);
-
-        if score > intermediate_score {
-            intermediate_score = score
-        }
-
-        rotate_array(&mut prop)
-    }
-
-    intermediate_score
-}
-
-/* ----------- */
+/* ---------- */
 
 fn main() {
-    let ingredients: Ingredients = utils::input_str!().lines().map(Ingredient::from).collect();
+    let gen = Partitions::<4>::new(100);
+    let ingrs: Vec<Stats> = INPUT.lines().map(Stats::from).collect();
+
     let mut max_score = 0;
+    let mut max_score_with_cals = 0;
 
-    let mut proportions = Partitions::new(100);
-    while let Some(prop) = proportions.next() {
-        if prop.len() == ingredients.len() {
-            let new_score = all_proportions_score(&ingredients, prop);
+    for parts in gen {
+        for teaspoons in Kombini::from(parts) {
+            let recipe = ingrs
+                .iter()
+                .enumerate()
+                .map(|(index, ingr)| *ingr * teaspoons[index])
+                .sum::<Stats>();
 
-            if new_score > max_score {
-                max_score = new_score
+            let score = recipe.score();
+
+            if score > max_score {
+                max_score = score
+            }
+
+            if recipe.calories() == 500 && score > max_score_with_cals {
+                max_score_with_cals = score
             }
         }
     }
 
-    println!("result = {}", max_score)
+    println!("[PART 1] Answer = {max_score}");
+    println!("[PART 2] Answer = {max_score_with_cals}");
 }

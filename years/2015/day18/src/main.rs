72b706463eda6f659e862407775af603105fa238
Maxime BAILLY <maxbailly@hotmail.fr>
2022-03-18 22:58:31 +0100

    2015 day 18 : completed !

--- years/2015/day18/src/main.rs
+++ years/2015/day18/src/main.rs
@@ -13,6 +13,24 @@ const fn grid_to_index((x, y): (usize, usize), size: usize) -> usize {
     y * size + x
 }
 
+const TOP_LEFT_ID: usize = grid_to_index((0, 0), ARRAY_LINE_SIZE);
+const TOP_RIGHT_ID: usize = grid_to_index((99, 0), ARRAY_LINE_SIZE);
+const BOT_LEFT_ID: usize = grid_to_index((0, 99), ARRAY_LINE_SIZE);
+const BOT_RIGHT_ID: usize = grid_to_index((99, 99), ARRAY_LINE_SIZE);
+
+/* ---------- */
+
+enum Part {
+    One,
+    Two,
+}
+
+impl Default for Part {
+    fn default() -> Self {
+        Self::One
+    }
+}
+
 /* ---------- */
 
 fn count_neighboor_on_at(array: &[LightState], idx: usize, size: usize) -> usize {
@@ -51,7 +69,7 @@ fn count_neighboor_on_at(array: &[LightState], idx: usize, size: usize) -> usize
 
 /* ---------- */
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 enum LightState {
     On,
     Off,
@@ -69,16 +87,22 @@ impl LightState {
 
 /* ---------- */
 
-fn next_step(prev: &[LightState], next: &mut Vec<LightState>) {
-    prev.iter().enumerate().for_each(|(idx, light)| {
-        let count = count_neighboor_on_at(prev, idx, ARRAY_LINE_SIZE);
-        next[idx] = light.next(count);
-    })
+fn next_step(prev: &[LightState], next: &mut Vec<LightState>, part: Part) {
+    prev.iter()
+        .enumerate()
+        .filter(|(index, _)| *index != TOP_LEFT_ID || !matches!(part, Part::Two))
+        .filter(|(index, _)| *index != TOP_RIGHT_ID || !matches!(part, Part::Two))
+        .filter(|(index, _)| *index != BOT_LEFT_ID || !matches!(part, Part::Two))
+        .filter(|(index, _)| *index != BOT_RIGHT_ID || !matches!(part, Part::Two))
+        .for_each(|(idx, light)| {
+            let count = count_neighboor_on_at(prev, idx, ARRAY_LINE_SIZE);
+            next[idx] = light.next(count);
+        })
 }
 
 /* ---------- */
 
-fn main() {
+fn part1() -> usize {
     let mut prev = utils::input_bytes!()
         .iter()
         .filter_map(|byte| match byte {
@@ -91,14 +115,54 @@ fn main() {
     let mut next = vec![LightState::Off; prev.len()];
 
     for _ in 0..STEPS {
-        next_step(&prev, &mut next);
+        next_step(&prev, &mut next, Part::One);
         next = std::mem::replace(&mut prev, next);
     }
 
-    let res = prev
+    prev
         .iter()
         .filter(|light| matches!(light, LightState::On))
-        .count();
+        .count()
+}
 
-    println!("result = {}", res);
+/* ---------- */
+
+fn part2() -> usize {
+    let mut prev = utils::input_bytes!()
+        .iter()
+        .filter_map(|byte| match byte {
+            b'#' => Some(LightState::On),
+            b'.' => Some(LightState::Off),
+            _ => None,
+        })
+        .collect::<Vec<LightState>>();
+
+    prev[TOP_LEFT_ID] = LightState::On;
+    prev[TOP_RIGHT_ID] = LightState::On;
+    prev[BOT_LEFT_ID] = LightState::On;
+    prev[BOT_RIGHT_ID] = LightState::On;
+
+    let mut next = vec![LightState::Off; prev.len()];
+    next[TOP_LEFT_ID] = LightState::On;
+    next[TOP_RIGHT_ID] = LightState::On;
+    next[BOT_LEFT_ID] = LightState::On;
+    next[BOT_RIGHT_ID] = LightState::On;
+
+    for _ in 0..STEPS {
+
+        next_step(&prev, &mut next, Part::Two);
+        next = std::mem::replace(&mut prev, next);
+    }
+
+    prev
+        .iter()
+        .filter(|light| matches!(light, LightState::On))
+        .count()
+}
+
+/* ---------- */
+
+fn main() {
+    // part2();
+    utils::answer!()
 }
